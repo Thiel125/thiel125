fdf4c6a2655884995a73e3e287185ae901813920
nwm <12345@12345.com>
2017-02-21 08:38:17 +0100

    pondeli 5.02

--- dohled.sh
+++ dohled.sh
@@ -4,8 +4,11 @@ cas=`date +%T" "%D`
 ip=`ifconfig | grep Všesměr | awk -F ":" '{print $2}' | awk -F " " '{print $1}'`
 rx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $2}' | awk -F ")" '{print $1}'`
 tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
+users=`who | wc -l`
 
-echo "Aktuální čas: "$cas
-echo "Moje IP: "$ip
-echo "Přijatá data: "$rx
-echo "Odeslaná data: "$tx
+echo "Aktuální čas: "$cas >> /home/student/Plocha/thiel125/index.html 
+echo "Moje IP: "$ip >> /home/student/Plocha/thiel125/index.html 
+echo "Přijatá data: "$rx >> /home/student/Plocha/thiel125/index.html 
+echo "Odeslaná data: "$tx >> /home/student/Plocha/thiel125/index.html 
+echo "počet přihlašených: "$users >> /home/student/Plocha/thiel125/index.html 
+echo "<br><br>"
