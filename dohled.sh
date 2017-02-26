4f64e87d3c8d63952fdec911d4cc6aa904878948
Thiel125 <adam.thiel1@seznam.cz>
2017-02-26 19:20:03 +0100

    Update dohled.sh

--- dohled.sh
+++ dohled.sh
@@ -5,10 +5,18 @@ ip=`ifconfig | grep Všesměr | awk -F ":" '{print $2}' | awk -F " " '{print $1}
 rx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $2}' | awk -F ")" '{print $1}'`
 tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
 users=`who | wc -l`
+pamet=`free | grep Mem: | awk -F " " '{print $3}'`
+disk=`df -h | grep /dev/sda1 | awk -F " " '{print $2}'`
+pouzito=`df -h | grep /dev/sda1 | awk -F " " '{print $3}'`
+procenta=`df -h | grep /dev/sda1 | awk -F " " '{print $5}'
 
-echo "Aktuální čas: "$cas >> /home/student/Plocha/thiel125/index.html 
-echo "Moje IP: "$ip >> /home/student/Plocha/thiel125/index.html 
-echo "Přijatá data: "$rx >> /home/student/Plocha/thiel125/index.html 
-echo "Odeslaná data: "$tx >> /home/student/Plocha/thiel125/index.html 
-echo "počet přihlašených: "$users >> /home/student/Plocha/thiel125/index.html 
-echo "<br><br>"
+echo "Current time: "$cas >> /home/student/Plocha/thiel125/index.html 
+echo "My Ip adress: "$ip >> /home/student/Plocha/thiel125/index.html 
+echo "Received data: "$rx >> /home/student/Plocha/thiel125/index.html 
+echo "Sent data: "$tx >> /home/student/Plocha/thiel125/index.html 
+echo "Member of users: "$users >> /home/student/Plocha/thiel125/index.html 
+echo "Used memory: "$pamet >> /home/student/Plocha/thiel125/index.html
+echo "Disk size: "$disk >> /home/student/Plocha/thiel125/index.html
+echo "Used space: "$pouzito >> /home/student/Plocha/thiel125/index.html
+echo "Percentage: "$procenta >> /home/student/Plocha/thiel125/index.html
+echo "<hr>" >> /home/student/Plocha/thiel125/index.html 
