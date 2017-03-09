9b72e736b4ca168ebe24429c594b9997b167274d
Thiel125 <adam.thiel1@seznam.cz>
2017-03-09 22:52:28 +0100

    Update dohled.sh

--- dohled.sh
+++ dohled.sh
@@ -8,16 +8,17 @@ users=`who | wc -l`
 memory=`free | grep Mem: | awk -F " " '{print $3}'`
 disk=`df -h | grep /dev/sda1 | awk -F " " '{print $2}'`
 used=`df -h | grep /dev/sda1 | awk -F " " '{print $3}'`
-procent=`df -h | grep /dev/sda1 | awk -F " " '{print $5}'
+procent=`df -h | grep /dev/sda1 | awk -F " " '{print $5}'`
 
 echo "<hr>" >> /home/student/Plocha/thiel125/index.html 
-echo "Current time: "$time >> /home/student/Plocha/thiel125/index.html 
-echo "My Ip adress: "$ip >> /home/student/Plocha/thiel125/index.html 
-echo "Received data: "$rx >> /home/student/Plocha/thiel125/index.html 
-echo "Sent data: "$tx >> /home/student/Plocha/thiel125/index.html 
-echo "Member of users: "$users >> /home/student/Plocha/thiel125/index.html 
-echo "Used memory: "$pmemory >> /home/student/Plocha/thiel125/index.html
-echo "Disk size: "$disk >> /home/student/Plocha/thiel125/index.html
-echo "Used space: "$used >> /home/student/Plocha/thiel125/index.html
-echo "Percentage: "$procent >> /home/student/Plocha/thiel125/index.html
+echo '<div style="font-family: arial; padding: 5px; text-align: center; background-colour: powderblue;">
+echo "<p>Current time: "$time"</p>" >> /home/student/Plocha/thiel125/index.html 
+echo "<p>My Ip adress: "$ip"</p>" >> /home/student/Plocha/thiel125/index.html 
+echo "<p>Received data: "$rx"</p>" >> /home/student/Plocha/thiel125/index.html 
+echo "<p>Sent data: "$tx"</p>" >> /home/student/Plocha/thiel125/index.html 
+echo "<p>Member of users: "$users"</p>" >> /home/student/Plocha/thiel125/index.html 
+echo "<p>Used memory: "$memory"</p>" >> /home/student/Plocha/thiel125/index.html
+echo "<p>Disk size: "$disk"</p>" >> /home/student/Plocha/thiel125/index.html
+echo "<p>Used space: "$used"</p>" >> /home/student/Plocha/thiel125/index.html
+echo "<p>Percentage: "$procent"</p>" >> /home/student/Plocha/thiel125/index.html
 echo "<hr>" >> /home/student/Plocha/thiel125/index.html 
