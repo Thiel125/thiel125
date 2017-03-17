663ed072efc32a044975b6e444048ebd6fdd6cdf
nwm <12345@12345.com>
2017-03-17 11:03:30 +0100

    17.3.

--- dohled.sh
+++ dohled.sh
@@ -11,14 +11,14 @@ used=`df -h | grep /dev/sda1 | awk -F " " '{print $3}'`
 procent=`df -h | grep /dev/sda1 | awk -F " " '{print $5}'`
 
 echo "<hr>" >> /home/student/Plocha/thiel125/index.html 
-echo '<div style="font-family: arial; padding: 5px; text-align: center; background-color: lightgreen; border: black 5px solid; margin: auto auto; width: 960px;">' >> /home/student/Plocha/thiel125/index.html
-echo '<h1 style="color:red; font-size: 50px; display:inline-block">' "<p><strong>Current time: "$time"</strong></h1></p>	" >> /home/student/Plocha/thiel125/index.html 
-echo '<h1 style="color:red; font-size: 50px; display:inline-block">' "<p><strong>My Ip adress: "$ip"</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html 
-echo '<h1 style="color:red; font-size: 50px; display:inline-block">' "<p><strong>Received data: "$rx"</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html 
-echo '<h1 style="color:red; font-size: 50px; display:inline-block">' "<p><strong>Sent data: "$tx"</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html 
-echo '<h1 style="color:red; font-size: 50px; display:inline-block">' "<p><strong>Member of users: "$users"</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html 
-echo '<h1 style="color:red; font-size: 50px; display:inline-block">' "<p><strong>Used memory: "$memory"B</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html
-echo '<h1 style="color:red; font-size: 50px; display:inline-block">' "<p><strong>Disk size: "$disk"B</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html
-echo '<h1 style="color:red; font-size: 50px; display:inline-block">' "<p><strong>Used space: "$used"B</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html
-echo '<h1 style="color:red; font-size: 50px; display:inline-block">' "<p><strong>Percentage: "$procent"</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html
+echo '<div style="font-family: arial; padding: 5px; text-align: center; background-color: lightblue; border: black 5px solid; margin: auto auto; width: 960px;">' >> /home/student/Plocha/thiel125/index.html
+echo '<h1 style="color:red; font-size: 25px; display:inline-block">' "<p><strong>Current time: "$time"</strong></h1></p>"  >> /home/student/Plocha/thiel125/index.html 
+echo '<h1 style="color:red; font-size: 25px; display:inline-block">' "<p><strong>My Ip adress: "$ip"</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html 
+echo '<h1 style="color:red; font-size: 25px; display:inline-block">' "<p><strong>Received data: "$rx"</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html 
+echo '<h1 style="color:red; font-size: 25px; display:inline-block">' "<p><strong>Sent data: "$tx"</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html 
+echo '<h1 style="color:red; font-size: 25px; display:inline-block">' "<p><strong>Member of users: "$users"</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html 
+echo '<h1 style="color:red; font-size: 25px; display:inline-block">' "<p><strong>Used memory: "$memory"B</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html
+echo '<h1 style="color:red; font-size: 25px; display:inline-block">' "<p><strong>Disk size: "$disk"B</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html
+echo '<h1 style="color:red; font-size: 25px; display:inline-block">' "<p><strong>Used space: "$used"B</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html
+echo '<h1 style="color:red; font-size: 25px; display:inline-block">' "<p><strong>Percentage: "$procent"</strong></h1></p>" >> /home/student/Plocha/thiel125/index.html
 echo "<hr>" >> /home/student/Plocha/thiel125/index.html 
